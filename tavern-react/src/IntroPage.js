4d3b6a65d42a3024175acfc6b56bb6fb70d7bd24
Advait <advaitm10@gmail.com>
2020-12-12 19:52:38 -0600

    created stub intro and log in pages

---
+++ tavern-react/src/IntroPage.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+function IntroPage(props) {
+    return(
+
+    );
+}
+
+export default IntroPage;
--- tavern-react/src/NavBar.js
+++ tavern-react/src/NavBar.js
@@ -1,8 +1,12 @@
 import React, {useState} from 'react';
+import AppBar from '@material-ui/core/AppBar'
 
 function NavBar(props) {
+    const [loggedIn, setLogIn] = useState([]);
     return (
-        
+        <AppBar position = 'static'>
+
+        </AppBar>
     );
 }
 
