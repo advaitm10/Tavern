3fa44be186c0ba5df1848c5643948f91b8ee200c
Advait <advaitm10@gmail.com>
2020-12-17 20:48:18 -0600

    Finished AppBar setup, need to do CSS and actions for buttons

--- tavern-react/src/App.js
+++ tavern-react/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Provider } from 'react-redux'
-import store from 'store';
+import store from './store';
 
 import NavBar from './NavBar';
 
--- tavern-react/src/NavBar.js
+++ tavern-react/src/NavBar.js
@@ -1,12 +1,20 @@
 import React, {useState} from 'react';
-import AppBar from '@material-ui/core/AppBar'
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
 
 function NavBar(props) {
     const loggedIn = props.logIn;
+    const preventDefault = (event) => event.preventDefault();
     return (
         
         <AppBar position = 'static'>
-            
+            <Link href = "#" onClick = {preventDefault} color = "inherit">
+                Tavern
+            </Link>
+            <Button color="inherit">
+                Login
+            </Button>
         </AppBar>
     );
 }
