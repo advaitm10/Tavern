652a8855e42dedece15fe42fe6dc941b464e7015
Advait <advaitm10@gmail.com>
2020-12-19 16:25:01 -0600

    Finished NavBar visuals

--- tavern-react/src/NavBar.js
+++ tavern-react/src/NavBar.js
@@ -1,21 +1,43 @@
 import React, {useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    title: {
+        flexGrow: 1,
+        marginRight: {
+            marginRight: theme.spacing(2)
+        }
+    },
+    appBar: {
+        backgroundColor: '#363636'
+    }
+}));
 
 function NavBar(props) {
     const loggedIn = props.logIn;
     const preventDefault = (event) => event.preventDefault();
+    const classes = useStyles();
+
     return (
-        
-        <AppBar position = 'static'>
-            <Link href = "#" onClick = {preventDefault} color = "inherit">
-                Tavern
-            </Link>
-            <Button color="inherit">
-                Login
-            </Button>
-        </AppBar>
+        <div className={classes.root}>
+            <AppBar className={classes.appBar} position = 'static'>
+                <Toolbar>
+                    <Link className={classes.title} href = "#" onClick = {preventDefault} color = "inherit">
+                        Tavern
+                    </Link>
+                    <Button color="inherit">
+                        Login
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </div>
     );
 }
 
